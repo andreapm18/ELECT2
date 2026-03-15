7c5a768e4cbdc2f9189d22243a79211666bfb6f2
andreapm18 <andreapmaglalang@gmail.com>
2026-03-15 13:12:45 +0800

    Delete LoginSystem directory

--- LoginSystem/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace LoginSystem.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
